eb084e6c33d87b03d850e432d5d337e2923732ef
samanehesh <145700227+samanehesh@users.noreply.github.com>
2026-02-06 13:57:21 -0800

    common layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,45 @@
+// import type { Metadata } from "next";
+// import "./globals.css"
+// import { ThemeProvider } from '@/components/theme-provider'
+// import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
+// export const metadata: Metadata = {
+//   title: "Portfolio-Samaneh Heshmatzadeh",
+//   description: "Samaneh Heshmatzadeh portfolio",
+//   icons: {
+//     icon: "/images/sama.svg",  
+//     apple: "/images/sama.svg", 
+//   },
+// };
+
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en" suppressHydrationWarning>
+
+//       <body className="flex flex-col min-h-screen">
+//         <ThemeProvider>
+//           <Header />
+//             <main className="flex-1">
+//               {children}
+//             </main>          
+//           <Footer />
+//         </ThemeProvider>
+
+//       </body>
+//     </html>
+//   );
+// }
+
 import type { Metadata } from "next";
 import "./globals.css"
+import CommonLayout from '@/layouts/CommonLayout'; 
 import { ThemeProvider } from '@/components/theme-provider'
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+
 export const metadata: Metadata = {
   title: "Portfolio-Samaneh Heshmatzadeh",
   description: "Samaneh Heshmatzadeh portfolio",
@@ -21,13 +58,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
 
-      <body className="flex flex-col min-h-screen">
+      <body>
         <ThemeProvider>
-          <Header />
-            <main className="flex-1">
-              {children}
-            </main>          
-          <Footer />
+
+          <CommonLayout>{children}</CommonLayout>
         </ThemeProvider>
 
       </body>
